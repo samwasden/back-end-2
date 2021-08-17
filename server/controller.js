577cef180ce9825936a6ab72cf744065366bc933
Sam Wasden <samwasden@outlook.com>
2021-08-17 14:59:18 -0600

    Final

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,50 @@
 const houses = require('./db.json')
 
 module.exports = {
-    
+    getHouses: (req, res) => {
+        res.status(200).send(houses)
+    },
+
+    createHouse: (req, res) => {
+        let { address, price, imageURL } = req.body
+        let newHouse = {
+            id: +houses[houses.length-1].id + 1,
+            address,
+            price: +price,
+            imageURL
+        }
+        houses.push(newHouse)
+        res.status(200).send(houses)
+    },
+
+    updateHouse: (req, res) => {
+        let {id} = req.params;
+        let {type} = req.body;
+
+        let index = houses.findIndex(elem => elem.id === +id)
+
+        if (houses[index].price <= 0 && type === 'minus') {
+            res.status(400).send('You cannot lower the price of this unit anymore.')
+        } else if (houses[index].price <= 10000 && type === 'minus') {
+            houses[index].price = 0;
+            res.status(200).send(houses)
+        } else if (type === 'plus') {
+            houses[index].price += 10000;
+            res.status(200).send(houses)
+        } else if (type === 'minus') {
+            houses[index].price -= 10000;
+            res.status(200).send(houses)
+        } else {
+            res.status(400).send('Something bad happened')
+        }
+    },
+
+    deleteHouse: (req, res) => {
+        let {id} = req.params;
+        let index = houses.findIndex(elem => elem.id === +id)
+        houses.splice(index, 1)
+        res.status(200).send(houses);
+    }
+
+
 }
--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 
-const ctrl = require('controller.js');
+const ctrl = require('./controller.js');
+
+app.get('/api/houses', ctrl.getHouses)
+app.post('/api/houses', ctrl.createHouse)
+app.put('/api/houses/:id', ctrl.updateHouse)
+app.delete('/api/houses/:id', ctrl.deleteHouse)
 
 app.listen(4004, () => console.log("doing shit on port 4004."));
